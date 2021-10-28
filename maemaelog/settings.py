b0307b8cf9702221389d2a5312e9d7477e35eeb5
efete <eternalfete@gmail.com>
2021-10-29 00:56:39 +0900

    add tradestats app and marketdata app

--- maemaelog/settings.py
+++ maemaelog/settings.py
@@ -45,6 +45,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'account',
+    'tradelog',
+    'tradestats',
+    'marketdata',
 ]
 
 MIDDLEWARE = [
