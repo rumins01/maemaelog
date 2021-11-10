47b2b64245ed21aae97215321f27439e936b6bbc
seonmoon <gorbange@gmail.com>
2021-11-10 15:08:04 +0900

    추가 누락

--- tradelog/admin.py
+++ tradelog/admin.py
@@ -1,11 +1,4 @@
 from django.contrib import admin
-<<<<<<< HEAD
-from tradelog import models
-
-# Register your models here.
-admin.site.register(models.TradeLog)
-admin.site.register(models.Account)
-=======
 from .models import TradeLog, Account
 
 
@@ -13,4 +6,3 @@ from .models import TradeLog, Account
 admin.site.register(TradeLog)
 admin.site.register(Account)
 
->>>>>>> 1cbcb50f04f466ce069207cf3699714475267179
