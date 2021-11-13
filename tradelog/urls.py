4d85712b911998279d68fc81f0534a63e7684d53
rumins01 <prolooker15@gmail.com>
2021-11-14 02:48:27 +0900

    tradelog-soomin1

--- tradelog/urls.py
+++ tradelog/urls.py
@@ -5,5 +5,11 @@ app_name = 'tradelog'
 urlpatterns = [
     # TODO: 매매일지 대시보드 url - https://www.maemaelog.com/nickname/
     # path('<slug:nickname>/', name='dashboard'),
+    path('dashboard/',views.dashboard, name='dashboard'),
+    path('create/', views.create, name='create'),
+    path('detail/<int:tradelog_id>', views.detail, name="detail"),
+    path('update/<int:tradelog_id>', views.update, name="update"),
+    path('delete/<int:tradelog_id>', views.delete, name="delete"),
+
     
 ]
--- tradelog/views.py
+++ tradelog/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import TradeLog
+from .forms import CreatForm
 
-# Create your views here.
+def dashboard(request):
+    posts = TradeLog.objects.filter().order_by('-update_at')
+    return render(request, 'tradelog/dashboard.html', {'posts' : posts})
+
+def create(request):
+    if(request.method == 'POST'): 
+        form = CreatForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('tradelog:dashboard')
+    else:
+        form = CreatForm()
+    return render(request, 'tradelog/form_create.html', {'form':form})
+
+def detail(request, tradelog_id):
+    tradelog_detail = get_object_or_404(TradeLog, pk=tradelog_id)
+    return render(request, 'tradelog/detail.html', {'tradelog_detail':tradelog_detail,})
+
+def update(request, tradelog_id):
+    my_tradelog = get_object_or_404(TradeLog, pk=tradelog_id)
+    if request.method == 'POST':
+        update_form = CreatForm(request.POST, instance = my_tradelog)
+        if update_form.is_valid():
+            update_form.save()
+            return redirect('tradelog:dashboard')
+    else:
+        update_form = TradeLog(instance=my_tradelog)
+    
+    return render(request, 'tradelog/update.html', {'update_form' : update_form})
+
+def delete(request, tradelog_id):
+    my_tradelog = get_object_or_404(TradeLog, pk=tradelog_id)
+    my_tradelog.delete()
+    return redirect('tradelog:dashboard')
