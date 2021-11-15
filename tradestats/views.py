51063b4754a8538038e0ae4cdeb7a8f027bef94f
efete <eternalfete@gmail.com>
2021-11-15 11:49:38 +0900

    remove print output

--- tradestats/views.py
+++ tradestats/views.py
@@ -4,10 +4,10 @@ import pandas as pd
 
 # Create your views here.
 
-def mystock_rank(request, user_name=1):
-    mystock_rank_object = TradeLog.objects.filter(user=user_name)
+def mystock_rank(request):
+    mystock_rank_object = TradeLog.objects.filter(user=request.user)
     mystock_rank_raw1 = pd.DataFrame(mystock_rank_object.values())
-    mystock_rank_raw1.loc[mystock_rank_raw1['type'] == 'Sell', 'amount'] *= -1
+    mystock_rank_raw1.loc[mystock_rank_raw1['type'] == 'S', 'amount'] *= -1
     mystock_rank_raw2 = mystock_rank_raw1.groupby('name')['amount'].sum().sort_values(ascending=False)
     mystock_rank_raw3 = mystock_rank_raw2.head(10)
     mystock_rank_raw4 = pd.DataFrame(zip(mystock_rank_raw3.index,mystock_rank_raw3.values), columns=['name','amount'])
