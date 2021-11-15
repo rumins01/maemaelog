368532a87520888336014b9ac79cc05cf345a0ee
rumins01 <prolooker15@gmail.com>
2021-11-15 11:53:43 +0900

    tradelog-soomin2

--- tradelog/models.py
+++ tradelog/models.py
@@ -10,14 +10,14 @@ class TradeLog(models.Model):
         ('S', 'Sell')
     ]
 
-    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField("stock name", max_length=40)
     code = models.CharField("stock code", max_length=20)
     trade_at = models.DateTimeField("trade date", default=timezone.now())
     price = models.IntegerField("trade price")
     amount = models.IntegerField("trade volume")
-    # type = models.CharField("trade type", max_length=1, choices=TRADE_TYPE)
-    # account = models.ForeignKey('Account', on_delete=models.RESTRICT)
+    type = models.CharField("trade type", max_length=1, choices=TRADE_TYPE)
+    account = models.ForeignKey('Account', on_delete=models.RESTRICT)
     description = models.TextField("trade description")
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
